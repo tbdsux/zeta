801e594945f5e7843a0f85d77d56b805798857d9
TheBoringDude <iamcoderx@gmail.com>
2020-11-13 19:13:36 +0800

    Register Collection models to admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from dashboard.models.collections import Collections, Inclution, Stuff
+
+# register collection models
+admin.site.register(Collections)
+admin.site.register(Inclution)
+admin.site.register(Stuff)
