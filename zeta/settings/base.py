6ebc74a3f08465c8a8555ca15241815a30ff862a
TheBoringDude <iamcoderx@gmail.com>
2021-04-29 17:03:14 +0800

    up & reconfigure deps

--- zeta/settings/base.py
+++ zeta/settings/base.py
@@ -121,3 +121,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "public")
 # media files
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
